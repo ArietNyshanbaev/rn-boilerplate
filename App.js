ca45afd4d8904c2be04ec469f71c560fc9f16959
Ariet <ariet.nyshanbaev@iaau.edu.kg>
2019-01-05 07:14:13 +0300

    install RNN v2

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native with wix RNN!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
